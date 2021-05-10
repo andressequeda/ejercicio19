0e64d8c306b2d936725381c22e535c0fa8fbd362
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:24:41 -0500

    Update ejercicio19.py

--- ejercicio19.py
+++ ejercicio19.py
@@ -6,3 +6,32 @@ diez_mil  =  ronda ( dinero  /  10000 )
 veinte_mil  =  ronda ( dinero  /  20000 )
 cincuenta_mil  =  round ( dinero  /  50000 )
 cien_mil  =  ronda ( dinero  /  100000 )
+
+si  mil  <  1 :
+    print ( "No tiene billetes de mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de mil es de:" , mil )
+si  dos_mil  <  1 :
+    print ( "No tiene billetes de dos mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de dos mil es de:" , dos_mil )
+si  cinco_mil  <  1 :
+    print ( "No tiene billetes de cinco mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de cinco mil es de:" , cinco_mil )
+si  diez_mil  <  1 :
+    print ( "No tiene billetes de diez mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de diez mil es de:" , diez_mil )
+si  veinte_mil  <  1 :
+    print ( "No tiene billetes de veinte mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de veinte mil es de:" , veinte_mil )
+si  cincuenta_mil  <  1 :
+    print ( "No tiene billetes de cincuenta mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de cincuenta mil es de:" , cincuenta_mil )
+si  cien_mil  <  1 :
+    print ( "No tiene billetes de cien mil" )
+otra cosa :
+    print ( "la cantida minima de billetes de billetes de cien mil es de:" , cien_mil )
